dfdd49d64e7d4dda61a612dd198cea8a149c8921
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-07-19 07:59:25 +0330

    Add HoodQuestGame init

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
+from data_structures import Stack
+
+from graph import build_game_map
+
 class GameState:
     __slots__ = ("player_pos", "wolf_pos", "score")
 
@@ -8,3 +12,20 @@ class GameState:
 
     def clone(self):
         return GameState(self.player_pos, self.wolf_pos, self.score)
+
+class HoodQuestGame:
+    UNDO_PENALTY = -2
+    MATCH_SUGGESTED_REWARD = 3
+    ALT_VALID_MOVE_REWARD = 1
+    REACH_GOAL_BONUS = 5
+
+    def __init__(self):
+        self.graph, self.goal = build_game_map()
+        self.player_pos = None
+        self.wolf_pos = None
+        self.score = 0
+        self.undo_stack = Stack()
+        self.turn_number = 1
+        self.game_over = False
+        self.result = None
+        self._setup_initial_positions()
