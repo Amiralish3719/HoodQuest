c6f11522099b339c263ed29cff3758159e30feb2
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-07-28 02:21:46 +0330

    Add prompt helpers.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 DIVIDER = "=" * 60
-
 def print_header(title):
     print(DIVIDER)
     print(title.center(60))
     print(DIVIDER)
+
+def prompt(msg):
+    try:
+        return input(msg)
+    except (EOFError, KeyboardInterrupt):
+        print("\nExiting the program...")
+        sys.exit(0)
