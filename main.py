fb3f3eab3ddc95b48d0757ed31a3fb25510f2f05
Amin Mohammad Jabbari <amin.jabbari2006@gmail.com>
2026-07-28 03:01:30 +0330

    import UserSystem.

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def handle_sign_up(user_system: UserSystem):
     username = prompt("New username: ")
     password = prompt("Password: ")
     ok, message = user_system.sign_up(username, password)
-    print((">> " if ok else "!! ") + message)
+    print((">> " if not ok else "!! ") + message)
     print()
